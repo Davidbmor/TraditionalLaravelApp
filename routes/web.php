62411e092f17172d2d8fc82d97d758bf1101eda1
Davidbmor <dbarmor1901@ieszaidinvergeles.org>
2024-10-22 18:15:05 +0000

    Cambios

--- routes/web.php
+++ routes/web.php
@@ -2,9 +2,12 @@
 
 use App\Http\Controllers\MainController;
 use App\Http\Controllers\ProductController;
+use App\Http\Controllers\FurnitureController;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', [MainController::class, 'main'])-> name('main');
 Route::get('login', [MainController::class, 'login'])-> name('login');
 Route::get('logout', [MainController::class, 'logout'])-> name('logout');
 Route::resource('product', ProductController::class);
+//5 7 Rutas
+Route::resource('furniture', FurnitureController::class);
